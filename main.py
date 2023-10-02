c7dedca26df33a3f3ac4b7a34c5dfe165e899ddb
dmostowfi <you@example.com>
2023-10-02 10:42:05 -0400

    import methods from spotify_api

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# TO DO
+from spotify_api import SpotifyAPI
+
+#opens an instance of SpotifyAPI class
+spotify = SpotifyAPI()
+
+#retrieves an access token from Spotify 
+spotify.get_token()
+
+#returns a list of all Spotify genres 
+spotify.get_genres()
+
+#for each genre, add artists to a dictionary
+spotify.add_artists()
