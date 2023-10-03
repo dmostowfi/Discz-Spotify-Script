48181488319e549f4af5603d83e436d2ebccce4f
dmostowfi <you@example.com>
2023-10-03 11:33:09 -0400

    make run_spotify_scraper async

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
-from spotify_api import SpotifyAPI
+import asyncio
+from spotify_api import SpotifyScraper
 
-try:
+async def run_spotify_scraper():
+    try:
 
-    #opens an instance of SpotifyAPI class
-    spotify = SpotifyAPI()
+        #opens an instance of SpotifyAPI class
+        spotify = SpotifyScraper()
 
-    #retrieves an access token from Spotify 
-    spotify.get_token()
+        #retrieves an access token from Spotify 
+        spotify.get_token()
 
-    #returns a list of all Spotify genres 
-    spotify.get_genres()
+        #testing async functionality
+        await spotify.get_genres()
+        await spotify.get_niche_genres()
+        await spotify.get_categories()
 
-    #for each genre, add artists to a dictionary
-    spotify.add_artists()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+if __name__ == '__main__':
+    asyncio.run(run_spotify_scraper())
+
+
+#await asyncio.gather(
+#    spotify.get_genres(),
+#    spotify.get_categories()
+#)
+
+#await asyncio.gather(
+#    spotify.add_artists(spotify.genres_list, 'genre'),
+#    spotify.add_artists(spotify.categories_list, 'category')
+#)
+
+#        task_genres = asyncio.create_task(spotify.get_genres())
+       # task_categories = asyncio.create_task(spotify.get_categories())
+
+       # await task_categories
+      #  task_add_artists_categories = asyncio.create_task(spotify.add_artists(spotify.categories_list, 'category'))
+
+      #  await task_genres
+      #  task_add_artists_genres = asyncio.create_task(spotify.add_artists(spotify.genres_list, 'genre'))
+
+     #   await asyncio.gather(task_add_artists_genres, task_add_artists_categories)
